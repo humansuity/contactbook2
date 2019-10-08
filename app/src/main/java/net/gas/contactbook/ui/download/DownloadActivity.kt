3f559c9ff73c1fbbfee8118abad6429ef0290a48
Дудкевич Валентин Дмитриевич <dudkevichvd@oblgas.by>
2019-10-08 17:09:35 +0300

    start download base

---
+++ app/src/main/java/net/gas/contactbook/ui/download/DownloadActivity.kt
@@ -0,0 +1,77 @@
+package net.gas.contactbook.ui.download
+
+import android.Manifest
+import android.app.DownloadManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Network
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.contactbook.R
+import kotlinx.android.synthetic.main.activity_download.*
+
+
+class DownloadActivity : AppCompatActivity() {
+
+    private val STORAGE_PERMISSION_CODE: Int= 1000
+
+        //Нужно переделать фунцию, чтобы устанавливала на внутренюю память, поправить Manifest
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_download)
+        downloadBtn.setOnClickListener{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED) {
+                    requestPermissions (arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+                }
+                else{
+                    startDownloading()
+                }
+            }
+            else {
+                startDownloading()
+            }
+        }
+    }
+
+    //Должно выкачивать базу, но нет!!!
+    private fun startDownloading(){
+        val url = "http://contactbook.oblgaz/contacts.db"
+        val request = DownloadManager.Request(Uri.parse(url))
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+        request.setTitle("Download")
+        request.setDescription("The base is downloading ...")
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOCUMENTS, "${System.currentTimeMillis()}")
+
+        val manager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        manager.enqueue(request)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+       when(requestCode){
+           STORAGE_PERMISSION_CODE -> {
+               if (grantResults.isNotEmpty() && grantResults[0] ==
+                           PackageManager.PERMISSION_DENIED) {
+                   startDownloading()
+               }
+               else{
+                   Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show()
+               }
+           }
+       }
+    }
+
+    //fun toastMe(view: View) {
+      //  val myToast = Toast.makeText(this, "Начинается загрузка", Toast.LENGTH_SHORT )
+        //myToast.show()
+
+}
+
