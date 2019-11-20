54cbe9422d24312d056219ed8b22a53c6974ad4c
velikoborecami <velikoborecami@oblgas.by>
2019-11-20 10:55:06 +0300

    add httpconnection check

--- app/src/main/java/net/gas/contactbook/ui/download/DataBaseDownloadTask.kt
+++ app/src/main/java/net/gas/contactbook/ui/download/DataBaseDownloadTask.kt
@@ -6,18 +6,21 @@ import android.content.Context
 import android.graphics.Color
 import android.os.AsyncTask
 import android.os.Build
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import com.example.contactbook.R
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
+import java.net.ConnectException
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.Exception
 
 class DataBaseDownloadTask(private val context: Context) : AsyncTask<Void, Void, Void>() {
 
     private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     private var notificationBuilder: NotificationCompat.Builder? = null
     private val notificationID = 1
+    private var isConnected = true
 
 
     override fun onPreExecute() {
@@ -29,39 +32,33 @@ class DataBaseDownloadTask(private val context: Context) : AsyncTask<Void, Void,
 
 
     override fun doInBackground(vararg params: Void?): Void? {
-
-        val url = URL("http://contactbook.oblgaz/contacts.db")
-        val connection = url.openConnection() as HttpURLConnection
-        connection.requestMethod = "GET"
-        connection.connect()
-
-        val path = File(context.filesDir.toURI())
-        val fullPath = File(path, url.path.split("/").last())
-        val fileOutputStream = FileOutputStream(fullPath)
-        val inputStream = connection.inputStream
-        val buffer = ByteArray(1024)
-
-
-        while(true) {
-            val len = inputStream.read(buffer)
-
-            if (len == -1) {
-                break
-            } else {
-                fileOutputStream.write(buffer, 0, len)
-            }
+        try {
+            val url = URL("http://contactbook.oblgaz/contacts.db")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            connection.readTimeout = 10*1000     //set 5 seconds to get response
+            connection.connect()
+            downloadViaHttpConnection(connection, url)
+        } catch (e: ConnectException) {
+            isConnected = false
+            return null
         }
 
         return null
-
-
     }
 
     
     override fun onPostExecute(result: Void?) {
         super.onPostExecute(result)
+
         //showing final notification
-        showNotification("Database downloaded!", false)
+        if (isConnected) {
+            Toast.makeText(context, "Database downloaded", Toast.LENGTH_LONG).show()
+            showNotification("Database downloaded!", false)
+        } else {
+            Toast.makeText(context, "Не удалось скачать базу данных", Toast.LENGTH_LONG).show()
+            showNotification("Unable to download database", false)
+        }
     }
 
 
@@ -91,6 +88,36 @@ class DataBaseDownloadTask(private val context: Context) : AsyncTask<Void, Void,
                 .setSmallIcon(R.drawable.database_download_ic)
                 .setProgress(0, 0, true)
         }
+    }
+
+
+    private fun downloadViaHttpConnection(connection: HttpURLConnection, url: URL) {
+        var inputStream: InputStream? = null
+        var fileOutputStream: FileOutputStream? = null
+        try {
+            val buffer = ByteArray(1024)
+            val path = File(context.filesDir.toURI())
+            val fullPath = File(path, url.path.split("/").last())
+            fileOutputStream = FileOutputStream(fullPath)
+            inputStream = connection.inputStream
+
+            while(true) {
+                val len = inputStream.read(buffer)
+
+                if (len == -1) {
+                    break
+                } else {
+                    fileOutputStream.write(buffer, 0, len)
+                }
+            }
+        } catch (e: Exception) {
+            isConnected = false
+            fileOutputStream?.close()
+            inputStream?.close()
+        }
+
+
+
     }
 
 
