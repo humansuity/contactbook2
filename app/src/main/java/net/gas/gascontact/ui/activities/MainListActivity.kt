a04496854e66509623638edd39592df3df2cd0f9
velikoborecami <velikoborecami@oblgas.by>
2020-04-03 14:25:38 +0300

    test merge

--- app/src/main/java/net/gas/gascontact/ui/activities/MainListActivity.kt
+++ app/src/main/java/net/gas/gascontact/ui/activities/MainListActivity.kt
@@ -147,6 +147,8 @@ class MainListActivity : AppCompatActivity() {
 
         viewModel.initUnitFragmentCallback = {
             createUnitListFragment()
+
+            /////////////////
         }
 
         viewModel.onReceiveDatabaseSizeCallBack = {
