1a35a5d5cac466d88b924133a606652ac38ac1d5
velikoborecami <velikoborecami@oblgas.by>
2020-05-08 16:00:33 +0300

    service remake

---
+++ app/src/main/java/net/gas/gascontact/business/NotificationService.kt
@@ -0,0 +1,12 @@
+package net.gas.gascontact.business
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class NotificationService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
