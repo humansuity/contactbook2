02de24b8bac4f7ecf3fd84d3312e8f8e0b40de18
velikoborecami <velikoborecami@oblgas.by>
2020-05-15 10:10:08 +0300

    update git configs

---
+++ app/src/main/java/net/gas/gascontact/business/BirthdayAlarmReceiver.kt
@@ -0,0 +1,13 @@
+package net.gas.gascontact.business
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BirthdayAlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("BirthdayAlarmReceiver.onReceive() is not implemented")
+    }
+}
